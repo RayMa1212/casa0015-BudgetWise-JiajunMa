5f421a6bf55ee077e48b652dde5c6f2232b24489
Jiajun Ma <jiajun.ma.23@ucl.ac.uk>
2024-04-23 04:51:37 +0100

    remove usedless dependance

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:core';
 
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+// import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
